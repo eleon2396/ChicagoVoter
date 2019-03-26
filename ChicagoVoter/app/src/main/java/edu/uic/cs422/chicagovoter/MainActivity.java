c9f91576f5f9c30e3113c3307d27589c22323080
Alexis <aurqui7@uic.edu>
2019-03-26 16:15:34 -0500

    added settings activity

--- ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/MainActivity.java
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.uic.cs422.chicagovoter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
     {
         getMenuInflater().inflate(R.menu.action_items, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.setting_button:
+                goToSettings();
+                break;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToSettings()
+    {
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
     }
 
     @Override
