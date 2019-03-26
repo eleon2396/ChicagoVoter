701a56031308dd58f96045c0f294692122b13b23
Alexis <aurqui7@uic.edu>
2019-03-26 15:06:36 -0500

    added toolbar and subtexts

--- ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/MainActivity.java
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/MainActivity.java
@@ -2,12 +2,27 @@ package edu.uic.cs422.chicagovoter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.action_items, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar chicagoToolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(chicagoToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
+
+
 }
