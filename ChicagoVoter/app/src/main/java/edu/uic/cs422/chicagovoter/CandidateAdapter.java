c2acc295b1a4d1b9e44079654662caa016a4c395
Alexis <aurqui7@uic.edu>
2019-03-29 18:58:26 -0500

    i lost track of what i did, but I added features to current events

---
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/CandidateAdapter.java
@@ -0,0 +1,66 @@
+package edu.uic.cs422.chicagovoter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CandidateAdapter extends BaseAdapter
+{
+    private List<Candidates> candidatesList;
+    private Context pContext;
+
+    public CandidateAdapter(List<Candidates> candidatesList, Context pContext)
+    {
+        this.candidatesList = candidatesList;
+        this.pContext = pContext;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return candidatesList.size();
+    }
+
+    @Override
+    public long getItemId(int pos)
+    {
+        return pos;
+    }
+
+    @Override
+    public Candidates getItem(int i)
+    {
+        return candidatesList.get(i);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+
+        // reuse and recycle
+        if(convertView == null)
+            convertView = LayoutInflater.from(pContext).inflate(R.layout.candidates_layout, parent, false);
+
+        ImageView candidateFace = (ImageView) convertView.findViewById(R.id.candidate_image);
+        TextView candidateName = (TextView) convertView.findViewById(R.id.candidate_name_text_view);
+        TextView candidateParty = (TextView) convertView.findViewById(R.id.candidate_party_pos);
+        TextView candidateContact = (TextView) convertView.findViewById(R.id.candidate_contact_info);
+        TextView candidateWebsite = (TextView) convertView.findViewById(R.id.candidate_website);
+
+        // add items to the listview
+        Candidates item = candidatesList.get(position);
+        candidateFace.setImageResource(item.imgId);
+        candidateName.setText(item.name);
+        candidateParty.setText(item.partyAffiliation);
+        candidateContact.setText(item.contact);
+        candidateWebsite.setText(item.website);
+
+        return convertView;
+    }
+}
--- ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/CurrentEventsActivity.java
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/CurrentEventsActivity.java
@@ -5,8 +5,34 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CurrentEventsActivity extends AppCompatActivity {
+
+    private ArrayList<Integer> candidateImages = new ArrayList<Integer>(Arrays.asList(
+            R.drawable.alexander_acavedo, R.drawable.byron_lopez, R.drawable.melissa_ervin, R.drawable.ameya_pawar,
+            R.drawable.toni_pre, R.drawable.lori_lightfoot));
+    private String[] candidateNames;
+    private String[] candidateRunningPositions;
+    private String[] candidatePartyAffil;
+    private String[] candidateContactInfo;
+    private String[] candidateWebsites;
+    private ArrayList<Candidates> candidatesList = null;
+    private CandidateAdapter candidateAdapter;
+
+    private String[] eventNames;
+    private String[] dates;
+    private String[] locations;
+    private String[] descriptions;
+    private ArrayList<Events> eventsLists = null;
+    private EventsAdapter eventAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +42,69 @@ public class CurrentEventsActivity extends AppCompatActivity {
         Toolbar chicagoToolbar = (Toolbar) findViewById(R.id.curr_toolbar);
         setSupportActionBar(chicagoToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // get strings for events going on in chicago
+        eventNames = getResources().getStringArray(R.array.event_name);
+        dates = getResources().getStringArray(R.array.event_date);
+        locations = getResources().getStringArray(R.array.event_location);
+        descriptions = getResources().getStringArray(R.array.description_events);
+
+        // populate raw data into an events object list
+        eventsLists = new ArrayList<Events>();
+        for(int i = 0; i < eventNames.length; i++)
+        {
+            Events item = new Events(eventNames[i], dates[i], locations[i], descriptions[i]);
+            eventsLists.add(item);
+        }
+
+        //get strings for candidates in chicago
+        candidateNames = getResources().getStringArray(R.array.candidate_names);
+        candidateRunningPositions = getResources().getStringArray(R.array.running_for_office);
+        candidatePartyAffil = getResources().getStringArray(R.array.candidate_political_party);
+        candidateContactInfo = getResources().getStringArray(R.array.candidate_contact_info);
+        candidateWebsites = getResources().getStringArray(R.array.candidate_website);
+
+        //populate raw data into candidate object list
+        candidatesList = new ArrayList<Candidates>();
+        for(int i = 0; i < candidateNames.length; i++)
+        {
+            Candidates item = new Candidates(candidateImages.get(i), candidateNames[i], candidateRunningPositions[i], candidatePartyAffil[i], candidateContactInfo[i], candidateWebsites[i]);
+            candidatesList.add(item);
+        }
+
+        // create custom adapters
+        eventAdapter = new EventsAdapter(eventsLists, this);
+        candidateAdapter = new CandidateAdapter(candidatesList, this);
+
+        ListView currEvents = (ListView) findViewById(R.id.current_events_listview);
+        currEvents.setAdapter(candidateAdapter);
+
+        setEventsListener();
+        setCandidatesListener();
+    }
+
+    public void setEventsListener()
+    {
+        Button eventBtn = (Button) findViewById(R.id.events_button);
+        eventBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView currEvents = (ListView) findViewById(R.id.current_events_listview);
+                currEvents.setAdapter(eventAdapter);
+            }
+        });
+    }
+
+    public void setCandidatesListener()
+    {
+        Button candidateBtn = (Button) findViewById(R.id.candidates_button);
+        candidateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView currEvents = (ListView) findViewById(R.id.current_events_listview);
+                currEvents.setAdapter(candidateAdapter);
+            }
+        });
     }
 
     @Override
