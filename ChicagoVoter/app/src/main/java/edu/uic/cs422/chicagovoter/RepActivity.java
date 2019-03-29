41dbd90bb70d36b8f2367f9d5a0f73d91cd64a5b
Alexis <aurqui7@uic.edu>
2019-03-28 21:03:44 -0500

    added list of reps

--- ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/RepActivity.java
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/RepActivity.java
@@ -2,12 +2,41 @@ package edu.uic.cs422.chicagovoter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RepActivity extends AppCompatActivity {
+
+    private ArrayList<Integer> facePicIDs = new ArrayList<Integer>(
+            Arrays.asList(R.drawable.chuy, R.drawable.rkelly, R.drawable.edburke, R.drawable.robmaldonado,
+                          R.drawable.rahm, R.drawable.brianhopkins, R.drawable.patdowell, R.drawable.sophiaking));
+    private ArrayList<Representatives> repInfo = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rep);
+
+        repInfo = new ArrayList<Representatives>();
+
+        String[] officeNames = getResources().getStringArray(R.array.office_name);
+        String[] names = getResources().getStringArray(R.array.rep_names);
+        String[] parties = getResources().getStringArray(R.array.political_party);
+        String[] websites = getResources().getStringArray(R.array.website);
+        String[] emails = getResources().getStringArray(R.array.email);
+        String[] phoneNumbers = getResources().getStringArray(R.array.phone_numer);
+        String[] reElection = getResources().getStringArray(R.array.is_running_again);
+
+        for(int i = 0; i < facePicIDs.size(); i++)
+        {
+            Representatives item = new Representatives(names[i], officeNames[i], facePicIDs.get(i), websites[i], emails[i], phoneNumbers[i], parties[i], reElection[i]);
+            repInfo.add(item);
+        }
+
+        ListView repList = (ListView) findViewById(R.id.rep_officials_listview);
+        repList.setAdapter(new RepAdapter(repInfo, this));
+
     }
 }
---
+++ ChicagoVoter/app/src/main/java/edu/uic/cs422/chicagovoter/RepAdapter.java
@@ -0,0 +1,86 @@
+package edu.uic.cs422.chicagovoter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RepAdapter extends BaseAdapter {
+
+    private List<Representatives> repInfo;
+    private Context pContext;
+
+    private TextView office;
+    private TextView name;
+    private TextView party;
+    private TextView website;
+    private TextView email;
+    private TextView phoneNumber;
+    private TextView reElection;
+    private ImageView repFace;
+
+    public RepAdapter(List<Representatives> repInfo, Context pContext)
+    {
+        this.repInfo = repInfo;
+        this.pContext = pContext;
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return repInfo.size();
+    }
+
+    @Override
+    public long getItemId(int pos)
+    {
+        return pos;
+    }
+
+    @Override
+    public Representatives getItem(int i)
+    {
+        return repInfo.get(i);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+
+        // reuse and recycle
+        if(convertView == null)
+            convertView = LayoutInflater.from(pContext).inflate(R.layout.rep_layout, parent, false);
+
+
+        repFace = (ImageView) convertView.findViewById(R.id.rep_image);
+        reElection = (TextView) convertView.findViewById(R.id.reelection_rep);
+        phoneNumber = (TextView) convertView.findViewById(R.id.phone_contact_rep);
+        email = (TextView) convertView.findViewById(R.id.email_rep);
+        website = (TextView) convertView.findViewById(R.id.website_rep);
+        party = (TextView) convertView.findViewById(R.id.political_party_rep);
+        name = (TextView) convertView.findViewById(R.id.official_name_rep);
+        office = (TextView) convertView.findViewById(R.id.office_name_rep);
+
+        // add items to the listview
+        Representatives item = repInfo.get(position);
+        repFace.setImageResource(item.imgID);
+        reElection.setText(item.reElection);
+        phoneNumber.setText(item.phoneNumber);
+        email.setText(item.email);
+        website.setText(item.website);
+        party.setText(item.party);
+        name.setText(item.repName);
+        office.setText(item.office);
+
+        return convertView;
+    }
+}
